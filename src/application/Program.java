69de8958066b0d3eeb305d5d4dd7a73437979105
Kaian Luciano <kaianlucianoalvesdossantos@gmail.com>
2022-12-22 19:41:49 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,18 @@
 package application;
 
 import java.text.ParseException;
-import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
-			
-		Department department = new Department(1, "Lindos");
-
-		Seller seller = new Seller(1, "Kaian", "kaian@gmail.com", new Date(), 2.000, department);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 		
