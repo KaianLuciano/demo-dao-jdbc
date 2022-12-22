23fe5d589146bd863b1070fd58a744d867a2222d
Kaian Luciano <kaianlucianoalvesdossantos@gmail.com>
2022-12-22 12:17:57 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,24 @@
 package application;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
+			
+		Department department = new Department(1, "Lindos");
+
+		Seller seller = new Seller(1, "Kaian", "kaian@gmail.com", new Date(), 2.000, department);
 		
-		SimpleDateFormat formater = new SimpleDateFormat("dd/MM/yyyy"); 
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Department dep = new Department(1, "Lindos");
-		System.out.println(dep);
-		
-		Seller sell = new Seller(1, "Kaian", "kaian@gmail.com", formater.parse("12/04/2003"), 2.000, new Department(1, "Jogos"));
-		System.out.println(sell);
+		System.out.println(seller);
 		
 	}
 
