f649f668d8774f553130e5aac749a02ce4e5f145
Kaian Luciano <kaianlucianoalvesdossantos@gmail.com>
2022-12-22 11:36:47 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department dep = new Department(1, "Lindos");
+		System.out.println(dep);
+	}
+
+}
