7962d44b98a67da8fca72355afc0e9f730195a33
Kaian Luciano <kaianlucianoalvesdossantos@gmail.com>
2022-12-23 17:02:08 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -10,8 +11,7 @@ import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
-		
+	public static void main(String[] args) throws ParseException {		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("===== TEST 1: Seller findbyID =====\n");
@@ -22,13 +22,15 @@ public class Program {
 		System.out.println("\n===== TEST 2: Seller findbyID =====\n");
 		Department dep = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(dep);
-		
 		list.forEach(System.out::println);
 		
 		System.out.println("\n===== TEST 3: Seller findAll =====\n");
 		list = sellerDao.findAll();
-		
 		list.forEach(System.out::println);
+		
+		System.out.println("\n===== TEST 3: Seller findAll =====\n");
+		Seller sellerTest = new Seller(1, "Enzo", "enzo@gmail.com", new Date(), 3.000, new Department(3, "Enzo"));
+		sellerDao.insert(sellerTest);
 	}
 
 }
